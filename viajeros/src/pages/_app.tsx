f1e3b91bce6b1cfc71fa995005675369a1176260
Franco Borrelli <franco@mimiquate.com>
2024-08-31 13:48:16 -0300

    feat(): add mantine and generated login page

--- viajeros/src/pages/_app.tsx
+++ viajeros/src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import type { AppProps } from 'next/app';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+  // We need to define what style we want for Viajeros!
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
--- viajeros/src/pages/_document.tsx
+++ viajeros/src/pages/_document.tsx
@@ -1,9 +1,11 @@
+import { ColorSchemeScript } from "@mantine/core";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <ColorSchemeScript defaultColorScheme="auto" />
       <body>
         <Main />
         <NextScript />
