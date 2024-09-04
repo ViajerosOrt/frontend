22fd854eefa66334365e8683006ed6f026f1d3c0
FabricioScarone <fafoscar30@hotmail.com>
2024-09-03 22:26:51 -0300

    feat(VIA-18): Login Changes

--- viajeros/src/pages/login.tsx
+++ viajeros/src/pages/login.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { TextInput, PasswordInput, Paper, Title, Container, Button, Stack, Text, BackgroundImage, Center } from '@mantine/core'
+import { useState } from 'react';
+import { TextInput, PasswordInput, Paper, Title, Container, Button, Stack, Text, BackgroundImage, Center, Group } from '@mantine/core';
 import { useRouter } from 'next/router';
-import { FaPlane, FaLock, FaEnvelope } from 'react-icons/fa'
+import { FaPlane, FaLock, FaEnvelope, FaUser } from 'react-icons/fa';
 
 export default function LoginPage() {
   const router = useRouter();
+  const [showSignUp, setShowSignUp] = useState(false);
 
   return (
     <BackgroundImage
@@ -20,43 +22,99 @@ export default function LoginPage() {
               <Title order={2} ta="center" fw={600} c="blue">
                 Viajeros
               </Title>
-              <Text size="sm" c="dimmed">Sign in to your account</Text>
+              {showSignUp ? ( //evalua la condicion para saber si mostrar el registro o login, en caso de showSignUp true, entonces mostramos registro
+                <>
+                  <Text size="sm" c="dimmed">Crea tu cuenta</Text>
+                  <form onSubmit={(e) => {
+                    e.preventDefault();
+                    setShowSignUp(false);
+                  }}>
+                    <Stack>
+                      <TextInput
+                        required
+                        label="Nombre"
+                        placeholder="Nombre"
+                        rightSection={<FaUser size="1rem" />}
+                        radius="md"
+                      />
+
+                      <TextInput
+                        required
+                        label="Email"
+                        placeholder="tu@email.com"
+                        rightSection={<FaEnvelope size="1rem" />}
+                        radius="md"
+                      />
+
+                      <PasswordInput
+                        required
+                        label="Contraseña"
+                        placeholder="Contraseña"
+                        rightSection={<FaLock size="1rem" />}
+                        radius="md"
+                      />
+
+                      <TextInput
+                        required
+                        label="Fecha de Nacimiento"
+                        placeholder="YYYY-MM-DD"
+                        type="date"
+                        radius="md"
+                      />
+                    </Stack>
+
+                    <Center mt="md"> 
+                      <Button type="submit" color="blue">Register</Button>
+                    </Center>
+                  </form>
+                  <Text ta="center" mt="md" size="sm">
+                    Ya tienes una cuenta? 
+                    <Text component="a" href="#" c="blue" onClick={() => setShowSignUp(false)}> 
+                      Login
+                    </Text>
+                  </Text>
+                </>
+              ) : (
+                <>
+                  <Text size="sm" c="dimmed">Sign in to your account</Text>
+                  <form onSubmit={(e) => {
+                    router.push("/viajes"); // Cuando el formulario se envia (le damos en login, vamos a la pagina de viajes)
+                    e.preventDefault();
+                  }}>
+                    <Stack>
+                      <TextInput
+                        required
+                        label="Email"
+                        placeholder="your@email.com"
+                        rightSection={<FaEnvelope size="1rem" />}
+                        radius="md"
+                      />
+
+                      <PasswordInput
+                        required
+                        label="Password"
+                        placeholder="Your password"
+                        rightSection={<FaLock size="1rem" />}
+                        radius="md"
+                      />
+                    </Stack>
+
+                    <Button type="submit" fullWidth mt="xl" color="blue">
+                      Sign in
+                    </Button>
+                  </form>
+                  <Text ta="center" mt="md" size="sm">
+                    No tienes cuenta?{' '}
+                    <Text component="a" href="#" c="blue" onClick={() => setShowSignUp(true)}>
+                      Registrate!
+                    </Text>
+                  </Text>
+                </>
+              )}
             </Stack>
-            <form onSubmit={(e) => {
-              router.push("/viajes")
-              e.preventDefault()
-            }}>
-              <Stack>
-                <TextInput
-                  required
-                  label="Email"
-                  placeholder="your@email.com"
-                  rightSection={<FaEnvelope size="1rem" />}
-                  radius="md"
-                />
-
-                <PasswordInput
-                  required
-                  label="Password"
-                  placeholder="Your password"
-                  rightSection={<FaLock size="1rem" />}
-                  radius="md"
-                />
-              </Stack>
-
-              <Button type="submit" fullWidth mt="xl" color="blue">
-                Sign in
-              </Button>
-            </form>
-            <Text ta="center" mt="md" size="sm">
-              Don&apos;t have an account?{' '}
-              <Text component="a" href="#" c="blue">
-                Sign up
-              </Text>
-            </Text>
           </Paper>
         </Container>
       </Center>
-    </BackgroundImage >
-  )
+    </BackgroundImage>
+  );
 }
