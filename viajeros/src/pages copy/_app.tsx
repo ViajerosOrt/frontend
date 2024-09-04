d7149a39532a6c796c664d71856255c8a72daa3e
FabricioScarone <fafoscar30@hotmail.com>
2024-09-03 22:23:38 -0300

    feat(VIA-18): Registry UI

---
+++ viajeros/src/pages copy/_app.tsx
@@ -0,0 +1,37 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import type { AppProps } from 'next/app';
+import { createTheme, MantineProvider } from '@mantine/core';
+import { useRouter } from 'next/router';
+import { AppContainer } from '@/components/AppContainer/AppContainer';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+  // We need to define what style we want for Viajeros!
+});
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // Routes that shouldn't have the Mantine AppShell layout.
+  // The other approach is to use per-page layouts
+  const noLayoutPages = ['/', '/login'];
+
+  // Check if the current route is one of the no layout pages
+  const isNoLayoutPage = noLayoutPages.includes(router.pathname);
+
+  return (
+    <MantineProvider theme={theme}>
+      {isNoLayoutPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <AppContainer>
+          <Component {...pageProps} />
+        </AppContainer>
+      )}
+    </MantineProvider>
+  );
+}
