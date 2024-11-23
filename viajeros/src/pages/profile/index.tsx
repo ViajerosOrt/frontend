3e59efccbb9c731019a302a8a5082eead4b8db84
Franco Borrelli <67347884+francoborr@users.noreply.github.com>
2024-11-23 19:49:05 -0300

    chore(1819): update (#13)

---
+++ viajeros/src/pages/profile/index.tsx
@@ -0,0 +1,12 @@
+import {
+  Container,
+  Title,
+} from "@mantine/core";
+
+export default function profile() {
+  return (
+    <Container size="xl" mt="xl">
+      <Title> Profile </Title>
+    </Container>
+  );
+}
