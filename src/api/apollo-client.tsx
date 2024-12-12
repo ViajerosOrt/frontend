047ce7df6810f614dd209bc271411b770615ff02
Solo-Bruno <brunolapazp@gmail.com>
2024-12-12 18:53:33 -0300

    feat(VIA-136): deploy 2

--- src/api/apollo-client.tsx
+++ src/api/apollo-client.tsx
@@ -4,6 +4,7 @@ import { setContext } from "@apollo/client/link/context";
 import { onError } from '@apollo/client/link/error'
 
 const backendApi = process.env.NEXT_PUBLIC_GRAPHQL_API_URL ?? "http://localhost:4000/graphql"
+console.log(backendApi)
 
 const httpLink = createHttpLink({
   uri: backendApi,
