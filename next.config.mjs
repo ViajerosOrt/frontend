26c7b3c8e0cbc721346216674fa81ee631433cc1
FabricioScarone <fafoscar30@hotmail.com>
2024-12-18 21:53:57 -0300

    intento definitivo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   env: {
     NEXT_PUBLIC_GRAPHQL_API_URL: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
   },
